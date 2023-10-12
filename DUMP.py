07edf1139901a0c43335da80122e5a856fe775b2
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-10-12 13:32:27 +0600

    Update DUMP.py

--- DUMP.py
+++ DUMP.py
@@ -1,14 +1,10 @@
 import os,platform
-
-
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
     import DUMPP
-else:
-    exit('\033[38;196m[×] Sorry Device Not Support')
+else:exit('\033[38;196m[×] Sorry Device Not Support')
 
  
 
