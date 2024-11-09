e56dd1b4aca6edddeda4171ad84e3d559a63771e
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2024-11-09 18:50:57 +0600

    Update DUMP.py

--- DUMP.py
+++ DUMP.py
@@ -2,6 +2,7 @@ import os,platform  #os.system('xdg-open https://facebook.com/groups/77061722729
 os.system('git pull')
 rmx = platform.architecture()[0]
 if rmx=='64bit':
-    import DMP3
+    #import DMP3
+    os.system("MAGICAL.py")
 elif rmx=='32bit':
     import DUMPO2
